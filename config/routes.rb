0b834e6e88b162e44d5ad8c1008081ff03101f80
Nathaniel Alba <nathaniel.alba@gmail.com>
2015-05-28 19:18:33 -0700

    finish signup forms and logins

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,14 @@ Rails.application.routes.draw do
   get 'pages/home'
   get 'pages/about'
   get 'pages/contact'
+
+  get '/cool'   => 'gif#cool'
+  get '/sweet'  => 'gif#sweet'
+
+  get '/signup'   => 'users#new'
+  post '/users' 	=> 'users#create'
+
+  get '/login'  => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
 end
